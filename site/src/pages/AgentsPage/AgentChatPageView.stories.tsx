a830117682fb8b038298026a8b50be4cc688d43f
Thomas Kosiewski <tk@coder.com>
2026-04-08 22:31:54 +0000

    feat(site): add Debug panel components and settings
    Change-Id: Idf1fe83a4235909fe5c143bf4afb81b2d89aebd6
    Signed-off-by: Thomas Kosiewski <tk@coder.com>

--- site/src/pages/AgentsPage/AgentChatPageView.stories.tsx
+++ site/src/pages/AgentsPage/AgentChatPageView.stories.tsx
@@ -137,6 +137,7 @@ const StoryAgentChatPageView: FC<StoryProps> = ({ editing, ...overrides }) => {
 		diffStatusData: undefined as ComponentProps<
 			typeof AgentChatPageView
 		>["diffStatusData"],
+		debugLoggingEnabled: false,
 		gitWatcher: buildGitWatcher(),
 		sshCommand: undefined as string | undefined,
 		handleCommit: fn(),
